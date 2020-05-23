020e84250b9d0af3b8ccc9b1e2d391bc1a812b0f
Matheus Delmondes <mdelmondes@icloud.com>
2020-05-23 18:42:37 -0300

    Melhorias de layout

--- screens/Login.js
+++ screens/Login.js
@@ -1,11 +1,29 @@
 import React from 'react'
-import { View, TextInput, StyleSheet, TouchableOpacity, Text, Button } from 'react-native'
+import { View, TextInput, StyleSheet, Text, Button, Image, TouchableHighlight, ActivityIndicator, KeyboardAvoidingView } from 'react-native'
 import { bindActionCreators } from 'redux'
 import { connect } from 'react-redux'
 import { updateEmail, updatePassword, login, getUser } from '../actions/user'
 import Firebase from '../config/Firebase'
 
 class Login extends React.Component {
+    constructor() {
+        super();
+        this.unsubscriber = null;
+        this.state = {
+            loading: false,
+        };
+    }
+
+    renderButtonOrLoading() {
+        if (this.state.loading) {
+            return <View style={styles.button}><ActivityIndicator /></View>
+        }
+        if (!this.props.user.email == '' && !this.props.user.password == '') {
+            return <TouchableHighlight id='btnAcessar' style={styles.button} onPress={() => { this.props.login(); this.setState({ loading: true }) }}><Text style={{ textAlign: 'center', color: '#fff', fontWeight: 'bold' }}>ACESSAR</Text></TouchableHighlight>;
+        }
+        return <TouchableHighlight style={styles.buttonDisabled} disabled={true} onPress={() => this.props.login()}><Text style={{ textAlign: 'center', color: '#fff', fontWeight: 'bold' }}>ACESSAR</Text></TouchableHighlight>;
+    }
+
     componentDidMount = () => {
         Firebase.auth().onAuthStateChanged(user => {
             if (user) {
@@ -19,69 +37,85 @@ class Login extends React.Component {
 
     render() {
         return (
-            <View style={styles.container}>
-                <TextInput
-                    style={styles.inputBox}
-                    value={this.props.user.email}
-                    onChangeText={email => this.props.updateEmail(email)}
-                    placeholder='Email'
-                    autoCapitalize='none'
-                />
-                <TextInput
-                    style={styles.inputBox}
-                    value={this.props.user.password}
-                    onChangeText={password => this.props.updatePassword(password)}
-                    placeholder='Password'
-                    secureTextEntry={true}
-                />
-                <TouchableOpacity style={styles.button} onPress={() => this.props.login()}>
-                    <Text style={styles.buttonText}>Entrar</Text>
-                </TouchableOpacity>
-                <Button
-                    title="Ainda não tem conta? Cadastre-se agora mesmo!"
-                    onPress={() => this.props.navigation.navigate('Signup')}
-                />
-            </View>
+            <KeyboardAvoidingView behavior={Platform.OS == 'ios' ? "padding" : "height"} style={{
+                flex: 1,
+                alignItems: 'center',
+                justifyContent: 'center'
+            }}>
+                <View style={styles.inputContainer}>
+                    <Image style={styles.img} resizeMode="contain" source={require('../images/logo.png')} />
+                    <TextInput
+                        style={styles.input}
+                        value={this.props.user.email}
+                        onChangeText={email => this.props.updateEmail(email)}
+                        placeholder='Email'
+                        autoCapitalize='none'
+                        keyboardType='email-address'
+                    />
+                    <TextInput
+                        style={styles.input}
+                        value={this.props.user.password}
+                        onChangeText={password => this.props.updatePassword(password)}
+                        placeholder='Senha'
+                        secureTextEntry={true}
+                    />
+
+                    {this.renderButtonOrLoading()}
+                    <Button
+                        title="Ainda não tem conta? Cadastre-se agora mesmo!"
+                        onPress={() => this.props.navigation.navigate('Signup')}
+                    />
+                </View>
+            </KeyboardAvoidingView>
         )
     }
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: '#fff',
-        alignItems: 'center',
-        justifyContent: 'center'
-    },
-    inputBox: {
-        width: '85%',
-        margin: 10,
+    input: {
         padding: 15,
-        fontSize: 16,
-        borderColor: '#d3d3d3',
-        borderBottomWidth: 1,
-        textAlign: 'center'
+        marginBottom: 20,
+        backgroundColor: '#fff',
+        borderRadius: 10,
+        borderColor: 'rgb(235,235,235)',
+        borderWidth: 1
+    },
+    inputContainer: {
+        paddingHorizontal: 20,
+        paddingVertical: 15,
+        borderRadius: 20,
+        backgroundColor: 'rgb(245,245,245)',
+        alignSelf: 'stretch',
+        marginHorizontal: 20,
+    },
+    img: {
+        alignSelf: 'center',
+        width: '70%',
+        marginBottom: 20
     },
     button: {
-        marginTop: 30,
-        marginBottom: 20,
-        paddingVertical: 5,
-        alignItems: 'center',
-        backgroundColor: '#F6820D',
-        borderColor: '#F6820D',
+        paddingTop: 20,
+        paddingBottom: 20,
+        backgroundColor: '#0a6ebd',
+        borderRadius: 10,
         borderWidth: 1,
-        borderRadius: 5,
-        width: 200
+        borderColor: '#fff'
     },
     buttonText: {
         fontSize: 20,
         fontWeight: 'bold',
-        color: '#fff'
+        color: '#fff',
+        textAlign: 'center'
     },
-    buttonSignup: {
-        fontSize: 12
-    }
-})
+    buttonDisabled: {
+        paddingTop: 20,
+        paddingBottom: 20,
+        backgroundColor: '#0a6ebd',
+        borderRadius: 10,
+        borderWidth: 1,
+        borderColor: '#fff',
+    },
+});
 
 const mapDispatchToProps = dispatch => {
     return bindActionCreators({ updateEmail, updatePassword, login, getUser }, dispatch)
--- screens/Signup.js
+++ screens/Signup.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { View, TextInput, StyleSheet, TouchableOpacity, Text } from 'react-native'
+import { Platform, Image, View, TextInput, StyleSheet, TouchableOpacity, Text, ActivityIndicator, TouchableHighlight, KeyboardAvoidingView } from 'react-native'
 import { bindActionCreators } from 'redux'
 import { connect } from 'react-redux'
 import { updateFullname, updateEmail, updatePassword, signup } from '../actions/user'
@@ -7,77 +7,97 @@ import { updateFullname, updateEmail, updatePassword, signup } from '../actions/
 class Signup extends React.Component {
     handleSignUp = () => {
         this.props.signup()
-        this.props.navigation.navigate('Profile')
+        this.props.navigation.navigate('Login')
     }
 
     render() {
         return (
-            <View style={styles.container}>
-                <TextInput
-                    style={styles.inputBox}
-                    value={this.props.user.fullname}
-                    onChangeText={fullname => this.props.updateFullname(fullname)}
-                    placeholder='Nome completo'
-                    autoCapitalize='none'
-                />
-                <TextInput
-                    style={styles.inputBox}
-                    value={this.props.user.email}
-                    onChangeText={email => this.props.updateEmail(email)}
-                    placeholder='Email'
-                    autoCapitalize='none'
-                />
-                <TextInput
-                    style={styles.inputBox}
-                    value={this.props.user.password}
-                    onChangeText={password => this.props.updatePassword(password)}
-                    placeholder='Senha'
-                    secureTextEntry={true}
-                />
-                <TouchableOpacity style={styles.button} onPress={this.handleSignUp}>
-                    <Text style={styles.buttonText}>Cadastrar</Text>
-                </TouchableOpacity>
-            </View>
+            <KeyboardAvoidingView behavior={Platform.OS == 'ios' ? "padding" : "height"} style={{
+                flex: 1,
+                alignItems: 'center',
+                justifyContent: 'center'
+            }}>
+                <View style={styles.inputContainer}>
+                    <Image style={styles.img} resizeMode="contain" source={require('../images/logo.png')} />
+                    <TextInput
+                        style={styles.input}
+                        value={this.props.user.fullname}
+                        onChangeText={fullname => this.props.updateFullname(fullname)}
+                        placeholder='Nome completo'
+                        autoCapitalize='none'
+                    />
+                    <TextInput
+                        style={styles.input}
+                        value={this.props.user.email}
+                        onChangeText={email => this.props.updateEmail(email)}
+                        placeholder='Email'
+                        autoCapitalize='none'
+                        keyboardType='email-address'
+                    />
+                    <TextInput
+                        style={styles.input}
+                        value={this.props.user.password}
+                        onChangeText={password => this.props.updatePassword(password)}
+                        placeholder='Senha'
+                        secureTextEntry={true}
+                    />
+                    <TouchableOpacity style={styles.buttonCadastro} onPress={this.handleSignUp}>
+                        <Text style={styles.buttonText}>Cadastrar</Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity style={styles.buttonVoltar} onPress={() => this.props.navigation.navigate('Login')}>
+                        <Text style={styles.buttonText}>Voltar</Text>
+                    </TouchableOpacity>
+                </View>
+            </KeyboardAvoidingView>
         )
     }
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: '#fff',
-        alignItems: 'center',
-        justifyContent: 'center'
-    },
-    inputBox: {
-        width: '85%',
-        margin: 10,
+    input: {
         padding: 15,
-        fontSize: 16,
-        borderColor: '#d3d3d3',
-        borderBottomWidth: 1,
-        textAlign: 'center'
-    },
-    button: {
-        marginTop: 30,
         marginBottom: 20,
-        paddingVertical: 5,
-        alignItems: 'center',
-        backgroundColor: '#FFA611',
-        borderColor: '#FFA611',
+        backgroundColor: '#fff',
+        borderRadius: 10,
+        borderColor: 'rgb(235,235,235)',
+        borderWidth: 1
+    },
+    inputContainer: {
+        paddingHorizontal: 20,
+        paddingVertical: 15,
+        borderRadius: 20,
+        backgroundColor: 'rgb(245,245,245)',
+        alignSelf: 'stretch',
+        marginHorizontal: 20,
+    },
+    buttonCadastro: {
+        paddingTop: 20,
+        paddingBottom: 20,
+        backgroundColor: '#0a6ebd',
+        borderRadius: 10,
         borderWidth: 1,
-        borderRadius: 5,
-        width: 200
+        borderColor: '#fff'
+    },
+    buttonVoltar: {
+        paddingTop: 20,
+        paddingBottom: 20,
+        backgroundColor: '#fca853',
+        borderRadius: 10,
+        borderWidth: 1,
+        borderColor: '#fff'
     },
     buttonText: {
         fontSize: 20,
         fontWeight: 'bold',
-        color: '#fff'
+        color: '#fff',
+        textAlign: 'center'
     },
-    buttonSignup: {
-        fontSize: 12
-    }
-})
+    img: {
+        alignSelf: 'center',
+        width: '70%',
+        marginBottom: 20
+    },
+});
 
 const mapDispatchToProps = dispatch => {
     return bindActionCreators({ updateFullname, updateEmail, updatePassword, signup }, dispatch)
